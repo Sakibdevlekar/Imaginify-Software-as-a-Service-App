97a5bd9e53387568f71142a162bbb954ac6c759e
sakibdevlekar <sakibdevlekar96@gmail.com>
2024-05-14 09:28:19 +0530

    dynamic form all feature completed

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,11 +1,35 @@
-import Header from "@/components/shared/Header";
-import { transformationTypes } from "@/constants/index";
+import Header from '@/components/shared/Header'
+import TransformationForm from '@/components/shared/TransformationFrom';
+import { transformationTypes } from '@/constants'
+import { getUserById } from '@/lib/actions/user.action';
+import { auth } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
 
-const AddTransformationTypePage = ({ params: { type } }: SearchParamProps) => {
+const AddTransformationTypePage = async ({ params: { type } }: SearchParamProps) => {
+  const { userId } = auth();
   const transformation = transformationTypes[type];
-  return (
-    <Header title={transformation.title} subtitle={transformation.subTitle} />
-  );
-};
 
-export default AddTransformationTypePage;
+  if(!userId) redirect('/sign-in')
+
+  const user = await getUserById(userId);
+
+  return (
+    <>
+      <Header 
+        title={transformation.title}
+        subtitle={transformation.subTitle}
+      />
+    
+      <section className="mt-10">
+        <TransformationForm 
+          action="Add"
+          userId={user._id}
+          type={transformation.type as TransformationTypeKey}
+          creditBalance={user.creditBalance}
+        />
+      </section>
+    </>
+  )
+}
+
+export default AddTransformationTypePage

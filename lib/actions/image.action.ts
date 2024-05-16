c673b20f5dba61cb6323782a0f882b8de24bb3e4
sakibdevlekar <sakibdevlekar96@gmail.com>
2024-05-16 16:45:19 +0530

    Home page with cloudinary search api completed

--- lib/actions/image.action.ts
+++ lib/actions/image.action.ts
@@ -6,6 +6,7 @@ import { handleError } from "../utils";
 import User from "../database/models/user.model";
 import Image from "../database/models/image.model";
 import { redirect } from "next/navigation";
+import { v2 as cloudinary } from "cloudinary";
 
 /*Add Image*/
 export async function addImage({ image, userId, path }: AddImageParams) {
@@ -67,10 +68,10 @@ export async function deleteImage(imageId: string) {
 }
 
 const populateUser = (query: any) =>
-    query.populate({
+  query.populate({
     path: "author",
     select: "_id firstName lastName",
-    });
+  });
 
 /*Get Image*/
 export async function getImageById(imageId: string) {
@@ -84,6 +85,69 @@ export async function getImageById(imageId: string) {
     }
 
     return JSON.parse(JSON.stringify(image));
+  } catch (error) {
+    handleError(error);
+  }
+}
+
+/* Get Images*/
+
+export async function getAllImages({
+  limit = 9,
+  page = 1,
+  searchQuery = " ",
+}: {
+  limit?: number;
+  page: number;
+  searchQuery?: string;
+}) {
+  try {
+    await connectToDatabase();
+
+    cloudinary.config({
+      cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+      api_key: process.env.CLOUDINARY_API_KEY,
+      api_secret: process.env.CLOUDINARY_API_SECRET,
+      secure: true,
+    });
+
+    let expression = "folder=imaginify";
+
+    if (searchQuery) {
+      expression += ` AND ${searchQuery}`;
+    }
+
+    const { resources } = await cloudinary.search
+      .expression(expression)
+      .execute();
+
+    const resourceIds = resources.map((resource: any) => resource.public_id);
+
+    let query = {};
+
+    if (searchQuery) {
+      query = {
+        publicId: {
+          $in: resourceIds,
+        },
+      };
+    }
+
+    const skipAmount = (Number(page) - 1) * limit;
+
+    const images = await populateUser(Image.find(query))
+      .sort({
+        updatedAt: -1,
+      })
+      .skip(skipAmount)
+      .limit(limit);
+    const totalImages = await Image.find(query).countDocuments();
+    const savedImages = await Image.find().countDocuments();
+    return {
+      data: JSON.parse(JSON.stringify(images)),
+      totalPages: Math.ceil(totalImages / limit),
+      savedImages: savedImages,
+    };
   } catch (error) {
     handleError(error);
   }
